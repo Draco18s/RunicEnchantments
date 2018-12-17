8278cba6bae2f0e1a8db84da2c4f477de4b0f3d8
Draco18s <draco18s@gmail.com>
2018-12-16 21:40:32 -0600

    Update Program.cs
    ...missing using

--- RunicInterpreter/Program.cs
+++ RunicInterpreter/Program.cs
@@ -4,6 +4,7 @@ using RunicInterpreter.draco18s.runic.init;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
