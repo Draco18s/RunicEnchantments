5c2a83f5edbf174e2d3f0e63b83b18a576f81410
I3\major.johnson <major.johnson@i3-corps.com>
2019-06-06 08:47:49 -0500

    Discovered that the command 'RA (random number) was calling `new Random()` every time it was executed, resulting in sequences such as `6'RA6'RA+` generating predominantly 0, 2, 4, 6, 8, and 10 in near equal measure (due to using system miliseconds as a seed).
    new Random() now invoked once.
    
    Added Factorial, Double Factorial, Primality Test, Digit Summation (43 -> 7), Digit Multiplication (43 -> 12) to the Math functions.

--- Assets/draco18s/runic/runes/RuneMathFunc.cs
+++ Assets/draco18s/runic/runes/RuneMathFunc.cs
@@ -1,9 +1,10 @@
-using Assets.draco18s.runic.init;
+﻿using Assets.draco18s.runic.init;
 using Assets.draco18s.util;
 using System;
 
 namespace Assets.draco18s.runic.runes {
 	class RuneMathFunc : IExecutableRune {
+		Random rand = new Random();
 		public bool Execute(Pointer pointer, ExecutionContext context) {
 			object o = pointer.Pop();
 			object v = pointer.Pop();
@@ -54,10 +55,69 @@ namespace Assets.draco18s.runic.runes {
 						pointer.Push(Math.Atan(x));
 						break;
 					case 'R':
-						Random rand = new Random();
 						pointer.Push(rand.Next((int)x));
 						break;
+					case '+':
+						char[] digits = x.ToString().ToCharArray();
+						int sum = 0;
+						foreach(char d in digits) {
+							sum += (d - 48);
+						}
+						pointer.Push(sum);
+						break;
+					case '*':
+						digits = x.ToString().ToCharArray();
+						sum = 1;
+						foreach(char d in digits) {
+							sum *= (d - 48);
+						}
+						pointer.Push(sum);
+						break;
+					case '!':
+						int number = (int)x;
+						if(number == 0) {
+							pointer.Push(1);
+							break;
+						}
+						double fact = number;
+						for(int i = number - 1; i >= 1; i--) {
+							fact *= i;
+						}
+						pointer.Push(fact);
+						break;
+					case '‼':
+						number = (int)x;
+						if(number == 0) {
+							pointer.Push(1);
+							break;
+						}
+						fact = number;
+						for(int i = number - 2; i >= 1; i -= 2) {
+							fact *= i;
+						}
+						pointer.Push(fact);
+						break;
+					case 'P':
+						pointer.Push(IsPrime((int)x) ? 1 : 0);
+						break;
 				}
+			}
+			return true;
+		}
+
+		private bool IsPrime(int number) {
+			if(number < 2)
+				return false;
+			if(number == 2)
+				return true;
+			if(number % 2 == 0)
+				return false;
+			if(number % 3 == 0)
+				return false;
+			double sq = Math.Sqrt(number);
+			for(int i = 5; i <= sq; i += 6) {
+				if(number % i == 0)
+					return false;
 			}
 			return true;
 		}
